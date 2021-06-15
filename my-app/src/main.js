1f2ab30f79efacc6595a00084e666b27fc9c5661
isaac-prog <panda_mm88@yahoo.com>
2021-06-15 04:14:21 +0000

    navbar added

---
+++ my-app/src/main.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.css'
+
+class MainPage extends React.Component{
+    render(){
+        return(
+            <React.Fragment> 
+                <div class="header">
+  <a href="#default"><img src="computerlogo.jpg" alt="img"/></a>
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a class="active" href="#contact">Add Content</a>
+    <a class="active" href="#about">About</a>
+  </div>
+</div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default MainPage
