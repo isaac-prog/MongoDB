5e2c325ffdc84e3ab97f8f5adb86aa7237c160df
isaac-prog <panda_mm88@yahoo.com>
2021-06-16 03:07:03 +0000

    added filter section

--- my-app/src/webPages/case.js
+++ my-app/src/webPages/case.js
@@ -2,21 +2,44 @@ import React from 'react'
 import './style.css'
 
 
-class casePage extends React.Component{
+class MainPage extends React.Component{
     render(){
         return(
             <React.Fragment> 
                 <div class="header">
-                    <a href="#default"><img src="computerlogo.jpg" alt="img"/></a>
-                <div class="header-right">
-                    <a class="active" href="#home">Home</a>
-                    <a class="active" href="#contact">Add Content</a>
-                    <a class="active" href="#about">About</a>
+                     <a href="#default"><img class='logo'src={require("./../images/computerlogo.jpg").default}/></a> 
+
+                    <ul class="nav-container">
+                    <li><a class="active" href="#home">Home</a></li>
+                    <li><a class="active" href="#contact">Add Content</a></li>
+                    <li><a class="active" href="#about">About</a></li>
+                    </ul>
+                    
                     </div>
-                    </div>
-            </React.Fragment>
+
+<div class="wallPaper">
+    <img class="image_center" src={require("./../images/computerCase.jpg").default}/>
+</div> 
+
+<table>
+  <tr>
+    <th></th>
+    <th>Name</th>
+    <th>Type</th> 
+    <th>Storage</th>
+    <th>Brand</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+</table>
+
+
+    </React.Fragment>
         )
     }
 }
 
-export default casePage
+export default MainPage
